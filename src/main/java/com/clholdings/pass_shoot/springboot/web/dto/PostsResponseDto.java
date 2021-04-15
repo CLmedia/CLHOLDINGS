e28ef11eaccb899d971b40bfc3477383377f3ea8
sykim86 <sykim8611@gmail.com>
2021-04-16 08:40:10 +0900

    init commit

---
+++ src/main/java/com/clholdings/pass_shoot/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.clholdings.pass_shoot.springboot.web.dto;
+
+import com.clholdings.pass_shoot.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
